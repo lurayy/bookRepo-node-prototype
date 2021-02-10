530f3817ae36f90c7eb8690f592309d812d365dc
Kabir S. Tamari <kabirtamari@gmail.com>
2021-02-10 15:39:01 +0545

    added key authorization for get

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {} from './database.js';
+import {db ,dbQuery} from './database.js';
 import {router} from './routes.js';
 import body_parser from 'body-parser';
 import fs from 'fs';
@@ -7,13 +7,29 @@ import {dateTimeInString} from './utils.js';
 
 const app = express();
 
-app.use(function(req, res, next){
-    console.log(req.headers);
-    next();
+app.use(async function(req, res, next){
+    console.log(req.headers.authorization);
+    if (req.headers.authorization){
+        let result = await db.query(
+            dbQuery.Get(
+                dbQuery.Ref(dbQuery.Collection('vendors'), req.headers.authorization)
+            )
+        )
+        console.log("Result : ", result)
+        if (result){
+            next();
+        }else{
+            res.status(400).send('Invalid Authorization Key !')
+        }
+    }else{
+        res.status(400).send('Unauthorized Access !')        
+    }
 })
 
-
 app.use(body_parser.json());
+app.post('*', ()=>{
+    console.log("POST Happened.")
+})
 app.use(router);
 
 const PORT = 8000;
